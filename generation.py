fcec77d10b669d6220056d6835f421a0e265deb7
mohammadmolavi <mohammadmolavi976@gmail.com>
2025-05-24 14:47:29 +0330

    creating simple generation.py and testing it by main.py

---
+++ generation.py
@@ -0,0 +1,34 @@
+from together import Together
+from dotenv import load_dotenv
+import os 
+def build_prompt(query: str, context: str) -> str:
+    # context_block = "\n".join(f"- {text}" for text in retrieved_texts)
+    prompt = f"""You are a helpful assistant. Based on the information provided below, please answer the user's question accurately and thoroughly.
+
+[Retrieved Information]:
+"{context}"
+
+[User's Question]:
+"{query}"
+
+[Please provide a clear, complete, and well-explained answer in English. Add examples or further details if necessary.]
+"""
+    return prompt
+
+load_dotenv(dotenv_path=".env.local")
+load_dotenv()
+
+# print(os.getenv("TOGETHER_API_KEY"))
+client = Together(api_key=os.getenv("TOGETHER_API_KEY"))
+
+def generate(question, context):
+    prompt = build_prompt(question, context)
+    response = client.chat.completions.create(
+        model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0.7,
+        max_tokens=512
+        )
+    return response.choices[0].message.content
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from retrieval import retrieve
+from generation import generate
+
+
 
 question ="What is the population of France ?"
 retrieve_doc=retrieve(question)
-print(retrieve_doc)
+# print(retrieve_doc[0]['text'])
+
+answer = generate(question, retrieve_doc[0]['text'])
+print(f"Question: {question}")
+print(f"Answer: {answer}")
+
