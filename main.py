5daa3b8eda31d9aaeb5314845b5028a71d0109ab
mohammadmolavi <mohammadmolavi976@gmail.com>
2025-05-25 20:00:27 +0330

    web prototype is complete. :) next step: optimizing_ fine tuning and increasing accuracy

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def retrieve_question(request: QuestionRequest):
         retrieve_doc = retrieve(question)
         if not retrieve_doc:
             raise HTTPException(status_code=404, detail="No relevant documents found.")
-        
+        print(retrieve_doc)
         return {"question": question, "documents": retrieve_doc}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -53,7 +53,7 @@ def generate_answer(request: QuestionRequest,):
         generated_doc = generate(question,answer)
         if not generated_doc:
             raise HTTPException(status_code=404, detail="No relevant documents generated.")
-        
+        print("answer :     ",generated_doc)
         return {"question": request, "answer": answer}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
